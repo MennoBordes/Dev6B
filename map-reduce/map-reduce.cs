1b5f9e697680eb1276b93320106bb44d562d9935
Skillerde6de <0948157@hr.nl>
2019-01-31 17:01:38 +0100

    Added where

--- map-reduce/map-reduce.cs
+++ map-reduce/map-reduce.cs
@@ -7,12 +7,25 @@ namespace map_reduce
 {
   static class map_reduce
   {
-    public static IEnumerable<T2> Map<T1,T2>(IEnumerable<T1> collection, Func<T1,T2> transformation)
+    public static IEnumerable<T2> Map<T1, T2>(IEnumerable<T1> collection, Func<T1, T2> transformation)
     {
       T2[] result = new T2[collection.Count()];
       for (int i = 0; i < collection.Count(); i++)
       {
-          result[i] = transformation(collection.ElementAt(i));
+        result[i] = transformation(collection.ElementAt(i));
+      }
+      return result;
+    }
+
+    public static IEnumerable<T> Where<T>(IEnumerable<T> collection, Func<T, bool> condition)
+    {
+      List<T> result = new List<T>();
+      for (int i = 0; i < collection.Count(); i++)
+      {
+        if(condition(collection.ElementAt(i)))
+        {
+          result.Add(collection.ElementAt(i));
+        }
       }
       return result;
     }
