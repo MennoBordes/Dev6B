39328b7824799f9900c2c2d15f356a0267dfa201
Skillerde6de <0948157@hr.nl>
2019-01-31 15:58:26 +0100

    Init

---
+++ map-reduce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace map_reduce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
